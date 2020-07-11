8a22d539a15776965d2584013370f55cec628a9b
Aisha-Shokry <aishashokry58@gmail.com>
2020-07-11 21:05:34 +0200

    deploy v.2

---
+++ build/precache-manifest.926111aedd4c6f973abddc179da75981.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71808498b413cdb393bd8693ebdd9150",
+    "url": "/index.html"
+  },
+  {
+    "revision": "015897f3018d580de6de",
+    "url": "/static/css/main.a357a7d8.chunk.css"
+  },
+  {
+    "revision": "5cc12d73022364822909",
+    "url": "/static/js/2.9bff6b2e.chunk.js"
+  },
+  {
+    "revision": "35594cead54c01eb7f0c844839c7cf9e",
+    "url": "/static/js/2.9bff6b2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "015897f3018d580de6de",
+    "url": "/static/js/main.ceb6c0c0.chunk.js"
+  },
+  {
+    "revision": "6fee555d53aed59f48e4",
+    "url": "/static/js/runtime-main.71243c77.js"
+  }
+]);
